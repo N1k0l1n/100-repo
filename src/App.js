004aa04a3462119bd4a872ace5c5fb56d0942d85
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-09-12 11:49:49 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {IoMailOutline, IoChevronForwardCicle, IoStar, FaBehance, FaDribbble} from 'react-icons/fa';
+import {IconContext} from 'react-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className='logo-wrapper'>
+            dev<span>page</span>
+        </div>
+        <div className='menu-container'>
+            <span>
+              <IconContext.Provider className="icons_container" value={{color: "#000", size:"18px"}}>
+                    <div className='icon'><FaBehance/></div>
+                    <div className='icon'><FaDribbble/></div>
+              </IconContext.Provider>
+            </span>
+            <span>
+            <IconContext.Provider className="icons_container" value={{color: "#000", size:"18px"}}>
+                    <div className='icon'><FaBehance/></div>
+                    hello@example.com
+              </IconContext.Provider>
+            </span>
+            <span className='menu'>
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
       </header>
-    </div>
+
+      <div className='content-wrapper'>
+        <div className='left_content_wrapper'>
+            <h2>
+              <span>Design focused</span>
+              <span>startup, for startups.</span>
+            </h2>
+          <p>When, while lovely valley teems with vapor around<br/> meridian sun strikes the upper world.</p>
+
+        </div>
+        <div className='right_content_wrapper'></div>
+        </div>
+    </>
   );
 }
 
