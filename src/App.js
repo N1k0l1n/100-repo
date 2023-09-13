2a028051ca5a26ab46393850758af42ae6cc955e
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-09-13 09:25:52 +0200

    fixing problem with node-sas version

--- src/App.js
+++ src/App.js
@@ -1,22 +1,145 @@
-import "./App.css";
+import "./App.scss";
 import { FaBehance, FaDribbble } from "react-icons/fa";
 import { IoMailOutline, IoChevronForwardCircle, IoStar } from "react-icons/io5";
 import { IconContext } from "react-icons";
-import pic from './assets/images/bg.png';
 import Card from "./components/Card";
+import { motion } from "framer-motion";
+import pic from "./assets/images/bg.png";
+
+let easeing = [0.6, -0.05, 0.01, 0.99];
+
+const stagger = {
+  animate: {
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.2,
+      staggerDirection: 1,
+    },
+  },
+};
+
+const fadeInUp = {
+  initial: {
+    y: -60,
+    opacity: 0,
+    transition: {
+      duration: 0.6,
+      ease: easeing,
+    },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      delay: 0.5,
+      ease: easeing,
+    },
+  },
+};
+
+const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
+
+const firstName = {
+  initial: {
+    y: -20,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.04,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const lastName = {
+  initial: {
+    y: -20,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.04,
+      staggerDirection: 1,
+    },
+  },
+};
+
+const letter = {
+  initial: {
+    y: 400,
+  },
+  animate: {
+    y: 0,
+    transition: { duration: 1, ...transition },
+  },
+};
+
+const btnGroup = {
+  initial: {
+    y: -60,
+    opacity: 0,
+    transition: { duration: 0.6, ease: easeing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    animation: {
+      duration: 0.6,
+      ease: easeing,
+    },
+  },
+};
+const star = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.8, ease: easeing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    animation: {
+      duration: 0.6,
+      ease: easeing,
+    },
+  },
+};
+
+const header = {
+  initial: {
+    y: -60,
+    opacity: 0,
+    transition: { duration: 0.05, ease: easeing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    animation: {
+      duration: 0.6,
+      ease: easeing,
+    },
+  },
+};
 
 function App() {
   return (
-    <>
-      <header>
-        <div className="logo-wrapper">
-          dev<span>page</span>
-        </div>
-        <div className="menu-container">
-          <span>
+    <motion.div initial="initial" animate="animate">
+      <motion.header variants={stagger}>
+        <motion.div className="logo_wrapper" variants={header}>
+          dev<span>amit</span>
+        </motion.div>
+        <motion.div className="menu_container" variants={stagger}>
+          <motion.span variants={header}>
             <IconContext.Provider
-              className="icons_container"
-              value={{ color: "#000", size: "18px" }}
+              value={{
+                color: "#000",
+                size: "18px",
+                className: "icons_container",
+              }}
             >
               <div className="icon">
                 <FaBehance />
@@ -25,75 +148,191 @@ function App() {
                 <FaDribbble />
               </div>
             </IconContext.Provider>
-          </span>
-          <span>
-            <IconContext.Provider
-              className="icons_container"
-              value={{ color: "#000", size: "18px" }}
-            >
+          </motion.span>
+          <motion.span variants={header}>
+            <IconContext.Provider value={{ color: "#000", size: "18px" }}>
               <div className="icon">
-                <FaBehance />
+                <IoMailOutline />
               </div>
-              hello@example.com
+              hello@example.co
             </IconContext.Provider>
-          </span>
-          <span className="menu">
+          </motion.span>
+          <motion.span className="menu" variants={header}>
             <span></span>
             <span></span>
             <span></span>
-          </span>
-        </div>
-      </header>
+          </motion.span>
+        </motion.div>
+      </motion.header>
 
-      <div className="content-wrapper">
+      <motion.div
+        className="content_wrapper"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.3, ease: easeing }}
+      >
         <div className="left_content_wrapper">
-          <h2>
-            <span>Design focused</span>
-            <span>startup, for startups.</span>
-          </h2>
-          <p>
-            When, while lovely valley teems with vapor around
-            <br /> meridian sun strikes the upper world.
-          </p>
-          <div className="btn_group">
-            <div className="btn btn_primary">
+          <motion.h2>
+            <motion.span
+              variants={firstName}
+              initial="initial"
+              animate="animate"
+              className="first"
+            >
+              <motion.span variants={letter}>D</motion.span>
+              <motion.span variants={letter}>e</motion.span>
+              <motion.span variants={letter}>s</motion.span>
+              <motion.span variants={letter}>i</motion.span>
+              <motion.span variants={letter}>g</motion.span>
+              <motion.span variants={letter}>n</motion.span>
+              <motion.span variants={letter} className="second">
+                f
+              </motion.span>
+              <motion.span variants={letter}>o</motion.span>
+              <motion.span variants={letter}>c</motion.span>
+              <motion.span variants={letter}>u</motion.span>
+              <motion.span variants={letter}>s</motion.span>
+              <motion.span variants={letter}>e</motion.span>
+              <motion.span variants={letter}>d</motion.span>
+            </motion.span>
+            <motion.span
+              variants={lastName}
+              initial="initial"
+              animate="animate"
+              className="last"
+            >
+              <motion.span variants={letter}>S</motion.span>
+              <motion.span variants={letter}>t</motion.span>
+              <motion.span variants={letter}>a</motion.span>
+              <motion.span variants={letter}>r</motion.span>
+              <motion.span variants={letter}>t</motion.span>
+              <motion.span variants={letter}>u</motion.span>
+              <motion.span variants={letter}>p,</motion.span>
+              <motion.span variants={letter} className="second">
+                f
+              </motion.span>
+              <motion.span variants={letter}>o</motion.span>
+              <motion.span variants={letter}>r</motion.span>
+              <motion.span variants={letter} className="second">
+                s
+              </motion.span>
+              <motion.span variants={letter}>t</motion.span>
+              <motion.span variants={letter}>a</motion.span>
+              <motion.span variants={letter}>r</motion.span>
+              <motion.span variants={letter}>t</motion.span>
+              <motion.span variants={letter}>u</motion.span>
+              <motion.span variants={letter}>p</motion.span>
+              <motion.span variants={letter}>s.</motion.span>
+            </motion.span>
+          </motion.h2>
+
+          <motion.p variants={fadeInUp}>
+            When, while lovely valley teems with vapor around meand <br />
+            meridian sun strikes the upper impenetrable.
+          </motion.p>
+
+          <motion.div className="btn_group" variants={stagger}>
+            <motion.div
+              className="btn btn_primary"
+              variants={btnGroup}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
               Hire Me
-              <IconContext.Provider
-                className="icons_container"
-                value={{ color: "#000", size: "25px" }}
-              >
+              <IconContext.Provider value={{ color: "#14da8f", size: "25px" }}>
                 <IoChevronForwardCircle />
               </IconContext.Provider>
-            </div>
-            <div className="btn btn_secondary">Live Chat</div>
-          </div>
+            </motion.div>
+            <motion.div
+              className="btn btn_secondary"
+              variants={btnGroup}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              Live Chat
+            </motion.div>
+          </motion.div>
 
-          <div className="reviews_container">
-            <p className="total_review">64+ Reviews</p>
-            <IconContext.Provider
-                className="icons_container"
-                value={{ color: "#000", size: "18px" }}
+          <motion.div className="review_container" variants={stagger}>
+            <motion.p className="total_review" variants={star}>
+              64+ Reviews
+            </motion.p>
+            <IconContext.Provider value={{ color: "#fff", size: "18px" }}>
+              <motion.span
+                variants={star}
+                whileHover={{
+                  scale: 1.2,
+                  rotate: 180,
+                  borderRadius: "100%",
+                  cursor: "pointer",
+                }}
               >
-                <span><IoStar/></span>
-                <span><IoStar/></span>
-                <span><IoStar/></span>
-                <span><IoStar/></span>
-                <span><IoStar/></span>
-              </IconContext.Provider>
-              <p>More then 50+ people taking services.</p>
-          </div>
+                <IoStar />
+              </motion.span>
+              <motion.span
+                variants={star}
+                whileHover={{
+                  scale: 1.2,
+                  rotate: 180,
+                  borderRadius: "100%",
+                  cursor: "pointer",
+                }}
+              >
+                <IoStar />
+              </motion.span>
+              <motion.span
+                variants={star}
+                whileHover={{
+                  scale: 1.2,
+                  rotate: 180,
+                  borderRadius: "100%",
+                  cursor: "pointer",
+                }}
+              >
+                <IoStar />
+              </motion.span>
+              <motion.span
+                variants={star}
+                whileHover={{
+                  scale: 1.2,
+                  rotate: 180,
+                  borderRadius: "100%",
+                  cursor: "pointer",
+                }}
+              >
+                <IoStar />
+              </motion.span>
+              <motion.span
+                variants={star}
+                whileHover={{
+                  scale: 1.2,
+                  rotate: 180,
+                  borderRadius: "100%",
+                  cursor: "pointer",
+                }}
+              >
+                <IoStar />
+              </motion.span>
+            </IconContext.Provider>
+            <motion.p className="more_review" variants={star}>
+              More then 50+ people taking services.
+            </motion.p>
+          </motion.div>
         </div>
 
-        <div className="right_content_wrapper">
-        <img src={pic} alt="bg"/>
+        <motion.div className="right_content_wrapper">
+          <motion.img
+            src={pic}
+            alt="bg"
+            initial={{ x: 200, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.8 }}
+          />
+        </motion.div>
+      </motion.div>
 
-        </div>
-      </div>
-
-
-    <Card/>
-
-    </>
+      <Card />
+    </motion.div>
   );
 }
 
